791c634823675a2d65a17f8480b6b73f5fc1cf81
Pewh Gosh <pwr.gosh@gmail.com>
2017-10-04 12:03:40 +0700

    Update Time.js

--- src/Time.js
+++ src/Time.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
 
-import moment from 'moment/min/moment-with-locales.min';
+import moment from 'moment';
 
 export default class Time extends React.Component {
   render() {
